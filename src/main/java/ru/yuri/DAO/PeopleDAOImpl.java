f9f492592013e612ebb13b34ef077a7292473e5b
Yuri <79991621127@yandex.ru>
2021-04-27 14:33:31 +0300

    Готовое CRUD

--- src/main/java/ru/yuri/DAO/PeopleDAOImpl.java
+++ src/main/java/ru/yuri/DAO/PeopleDAOImpl.java
@@ -1,6 +1,5 @@
 package ru.yuri.DAO;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.yuri.model.Person;
 
--- src/main/java/ru/yuri/controllers/PeopleController.java
+++ src/main/java/ru/yuri/controllers/PeopleController.java
@@ -4,29 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.yuri.DAO.PeopleDAOImpl;
 import ru.yuri.model.Person;
-import ru.yuri.services.PersonServiceImpl;
+import ru.yuri.services.PersonService;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
-
-    private final PeopleDAOImpl peopleDAO;
-
-    public PeopleController(PeopleDAOImpl peopleDAO) {
-        this.peopleDAO = peopleDAO;
-    }
+    @Autowired
+    private PersonService service;
 
     @GetMapping()
     public String index(Model model) {
-        model.addAttribute("people", peopleDAO.index());
+        model.addAttribute("people", service.index());
         return "people/index";
     }
 
     @GetMapping("/{id}")
     public String get(@PathVariable("id") int id, Model model){
-        model.addAttribute("person", peopleDAO.get(id));
+        model.addAttribute("person", service.get(id));
         return "people/show";
     }
 
@@ -37,23 +32,23 @@ public class PeopleController {
     }
     @PostMapping()
     public String create(@ModelAttribute("person") Person person){
-        peopleDAO.save(person);
+        service.save(person);
         return "redirect:/people";
     }
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id){
-        model.addAttribute("person", peopleDAO.get(id));
+        model.addAttribute("person", service.get(id));
         return "people/edit";
     }
-    @PatchMapping()
+    @PatchMapping("/{id}")
     public  String update(@ModelAttribute("person") Person person){
-        peopleDAO.update(person);
+        service.update(person);
         return "redirect:/people";
     }
     @DeleteMapping("/{id}")
     public  String delete(@PathVariable("id") int id){
-        peopleDAO.delete(id);
+        service.delete(id);
         return "redirect:/people";
     }
 }
